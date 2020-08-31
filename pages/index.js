341fb835abf9be4a53b1de7286eddb11e1186ecb
Jesse Hull <jse.hull@gmail.com>
2020-08-31 13:27:59 -0600

    setup basic Firebase + GitHub auth

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useAuth } from '../lib/auth'
 
-export default function Home() {
+const Home = () => {
+  const auth = useAuth()
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback Demo</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Fast Feedback</h1>
+        <button onClick={e => auth.signinWithGithub()}>sign in</button>
+        <div>
+          <p>{auth?.user?.email}</p>
+        </div>
+        {auth?.user && <button onClick={e => auth.signout()}>sign out</button>}
       </main>
     </div>
   )
 }
+
+export default Home
